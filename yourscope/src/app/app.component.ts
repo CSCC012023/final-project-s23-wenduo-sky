0544759489f38cab80efe31561894edd32e8ae00
YungCos <milesbernstein48@gmail.com>
2023-06-01 20:48:13 -0400

    test: Firebase button now works

--- yourscope/src/app/app.component.ts
+++ yourscope/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Database, ref, set } from '@angular/fire/database';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'yourscope';
 
+  private database: Database = inject(Database);
+
   constructor() {}
 
   addData() {
-    
+    const test = ref(this.database, '/test')
+    const d = new Date();
+    let time = d.getTime();
+    set(test, {
+      currTime: time
+    })
+
   }
 }
