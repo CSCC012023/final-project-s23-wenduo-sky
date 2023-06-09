08a4181f59c02abc0009f041afd3eed592841b9a
Cosmo48 <59401027+Cosmo48@users.noreply.github.com>
2023-06-09 15:16:33 -0400

    feat: added routing and registration ui

--- yourscope/src/app/auth/register/register.component.ts
+++ yourscope/src/app/auth/register/register.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  public regState: number = 0;
+
+  loadStudentRegistraion(){
+    this.regState = 1;
+  }
+
+  loadEmployerRegistraion(){
+    this.regState = 2;
+  }
 
 }
