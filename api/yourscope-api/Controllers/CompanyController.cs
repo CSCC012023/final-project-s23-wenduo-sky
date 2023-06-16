d0d232adfaef49ffecd8af2f1972d511d2d3a57e
Jun <17263194+xzyio@users.noreply.github.com>
2023-06-16 14:17:33 -0400

    Added endpoint to check company existence (#24)
    * Fix gitignore
    
    * Revert "Fix gitignore"
    
    This reverts commit ef2a3cb1961fc11a5250b7c1e908efb2dfd42275.
    
    * Fix gitignore
    
    * cleaned up ignored files
    
    * Created interface to check company existance
    
    * Added dependency injection

---
+++ api/yourscope-api/Controllers/CompanyController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using yourscope_api.service;
+
+namespace yourscope_api.Controllers
+{
+    [Route("api/[controller]/v1")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        #region constructors and class fields
+        private readonly ICompanyService service;
+
+        public CompanyController(ICompanyService service)
+        {
+            this.service = service;
+        }
+        #endregion
+
+        [HttpGet]
+        [Route("check-company-exist/{company}")]
+        public IActionResult CheckCompanyExists(string company)
+        {
+            try
+            {
+                return Ok(service.CheckCompanyExists(company));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ api/yourscope-api/Services/CompanyService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using yourscope_api.Models.DbModels;
+
+namespace yourscope_api.service
+{
+    public class CompanyService : ICompanyService
+    {
+        #region constructors and class fields
+        public readonly string name;
+        public CompanyService(string name)
+        {
+            this.name = name;
+        }
+        #endregion
+        public bool CheckCompanyExists(string company)
+        {
+            using var context = new YourScopeContext();
+
+            List<Company> exist = context.Company.Where(comp => comp.CompanyName == company).ToList();
+            return exist.Count > 0;
+        }
+
+        public async Task<IActionResult> RegisterCompanyMethod()
+        {
+            return new CreatedResult("User successfully registered.", true);
+        }
+    }
+}
