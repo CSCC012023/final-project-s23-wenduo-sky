55ecc151b26376e8e5e303290a08eb1cfe8134a7
Cosmo48 <59401027+Cosmo48@users.noreply.github.com>
2023-06-26 21:37:32 -0400

    added job tables through efcore

---
+++ api/yourscope-api/Models/DbModels/JobPosting.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace yourscope_api.Models.DbModels
+{
+    public class JobPosting
+    {
+        [Key]
+        public int JobPostingId { get; set; }
+        public required User User { get; set; } = null!;
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public DateTime ApplicationDeadline { get; set; }
+        
+    }
+}
---
+++ api/yourscope-api/Models/DbModels/JobQuestion.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace yourscope_api.Models.DbModels
+{
+    public class JobQuestion
+    {
+        [Key]
+        public int JobQuestionId { get; set; }
+        public required JobPosting JobPosting { get; set; } = null!;
+        public required string Question { get; set; }
+        public int MaxWords { get; set; }
+    }
+}
