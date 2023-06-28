f05c8bc22b829c169917056c48c6529c87fd8018
Jason <jasonsu894@gmail.com>
2023-06-28 16:08:25 -0400

    refactor: Added ApiResponse object for consistent API returns.

---
+++ api/yourscope-api/Entities/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace yourscope_api.entities
+{
+    public class ApiResponse
+    {
+        public int StatusCode;
+        public object? Data;
+        public object[] Errors = Array.Empty<object>();
+    }
+}
