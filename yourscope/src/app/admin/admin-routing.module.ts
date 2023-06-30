3d1a0e4b5916980ad00ed393991b3da3e044e444
CodeMakesMeHowell <annachester02@gmail.com>
2023-06-30 15:40:01 -0400

    Feat: added UI for events and create event pages

--- yourscope/src/app/admin/admin-routing.module.ts
+++ yourscope/src/app/admin/admin-routing.module.ts
@@ -5,11 +5,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
 import { AdminCoursesComponent } from './admin-courses/admin-courses.component';
 import { AdminEventsComponent } from './admin-events/admin-events.component';
+import { CreateEventComponent } from './create-event/create-event.component';
 
 const routes: Routes = [
   {   path: 'admin',   component: DashboardAdminComponent},
   {   path: 'admin/courses',   component: AdminCoursesComponent},
-  {   path: 'admin/events',   component: AdminEventsComponent}
+  {   path: 'admin/events',   component: AdminEventsComponent},
+  {   path: 'admin/create-event', component: CreateEventComponent}
 ];
 
 @NgModule({
