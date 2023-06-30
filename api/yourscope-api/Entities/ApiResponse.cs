5de7adf7232112c8b24457a17fbca11739941ee9
Jason <jasonsu894@gmail.com>
2023-06-29 22:28:50 -0400

    refactor: Updated ApiResponse.cs class to include more fields and a constructor.

--- api/yourscope-api/Entities/ApiResponse.cs
+++ api/yourscope-api/Entities/ApiResponse.cs
@@ -2,8 +2,21 @@
 {
     public class ApiResponse
     {
-        public int StatusCode;
-        public object? Data;
-        public object[] Errors = Array.Empty<object>();
+        public int StatusCode { get; set; }
+        public object? Data { get; set; }
+        public IEnumerable<string>? Errors { get; set; }
+        public string? Message { get; set; }
+        public bool? Successful { get; set; }
+
+        #region constructors
+        public ApiResponse(int statusCode, string? message = null, object ? data = null, bool? success = null, IEnumerable<object>? errors = null)
+        {
+            this.StatusCode = statusCode;
+            this.Data = data;  
+            this.Message = message;
+            this.Errors = errors;
+            this.Successful = success;
+        }
+        #endregion
     }
 }
