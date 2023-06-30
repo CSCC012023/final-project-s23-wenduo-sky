311df3b14605014f81ed4c1b97af482284aed937
Jason <jasonsu894@gmail.com>
2023-06-30 18:15:46 -0400

    YS-78 - Created endpoints to delete and create events.

---
+++ api/yourscope-api/Services/EventsService.cs
@@ -0,0 +1,75 @@
+﻿using Firebase.Auth.Providers;
+using Firebase.Auth;
+using FirebaseAdmin;
+using yourscope_api.entities;
+using yourscope_api.Models.DbModels;
+using User = yourscope_api.Models.DbModels.User;
+
+namespace yourscope_api.service
+{
+    public class EventsService : IEventsService
+    {
+        #region class fields and constructors
+        private readonly IConfiguration configuration;
+
+        public EventsService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        #endregion
+
+        public async Task<ApiResponse> CreateEventMethod(EventCreationDto eventDetails)
+        {
+            using var context = new YourScopeContext();
+            // Retrieve the user object.
+            User? user = context.Users.Where(user => user.UserId == eventDetails.UserId).FirstOrDefault();
+            if (user is null)
+                return new ApiResponse(StatusCodes.Status404NotFound, $"User with id {eventDetails.UserId} does not exist.", success: false);
+
+
+            // Convert dto to Event object.
+            Event createdEvent = new()
+            {
+                Title = eventDetails.Title,
+                Description = eventDetails.Description,
+                Date = eventDetails.Date,
+                Location = eventDetails.Location,
+                User = user
+            };
+
+            // Insert into db.
+            context.Events.Add(createdEvent);
+            await context.SaveChangesAsync();
+            
+
+            return new ApiResponse(StatusCodes.Status201Created, "Event successfully created!", true);
+        }
+
+        public async Task<ApiResponse> DeleteEventMethod(int id)
+        {
+            using var context = new YourScopeContext();
+
+            // Ensure the event exists.
+            Event? toDelete = GetEventById(id);
+            if (toDelete is null)
+                return new ApiResponse(StatusCodes.Status404NotFound, $"Event with id {id} does not exist.", success: false);
+
+            // Removing the event from the db.
+            context.Events.Remove(toDelete);
+            await context.SaveChangesAsync();
+
+            return new ApiResponse(StatusCodes.Status200OK, "Event sucessfully deleted.", data: true, success: true);
+        }
+
+        #region helper methods
+        private Event? GetEventById(int id)
+        {
+            using var context = new YourScopeContext();
+
+            Event? result = context.Events.Where(e => e.EventId == id).FirstOrDefault();
+
+            return result;
+        }
+        #endregion
+    }
+}
