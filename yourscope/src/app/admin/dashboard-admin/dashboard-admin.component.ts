45f2574e1ed5d5f75e8048e12f86c1b768b2ebbf
CodeMakesMeHowell <annachester02@gmail.com>
2023-07-03 20:27:28 -0400

    YS-77: added UI and UI logic for creating/viewing/deleting events

--- yourscope/src/app/admin/dashboard-admin/dashboard-admin.component.ts
+++ yourscope/src/app/admin/dashboard-admin/dashboard-admin.component.ts
@@ -1,37 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { APIService } from 'src/app/services/api.service';
+import { JwtService } from 'src/app/services/jwt.service';
 @Component({
   selector: 'app-dashboard-admin',
   templateUrl: './dashboard-admin.component.html',
   styleUrls: ['./dashboard-admin.component.scss']
 })
-export class DashboardAdminComponent {
-  events = [
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-    {title: 'Event', url: '#'},
-  ];
+export class DashboardAdminComponent implements OnInit{
+  events = <any> [];
+
+  constructor(private hc: APIService, private cookie: CookieService, private jwtService: JwtService) { }
+
+  ngOnInit(): void {
+    let loginToken = this.cookie.get("loginToken");
+    let decodedToken = this.jwtService.DecodeToken(loginToken);
+    
+    this.hc.getEvents(0,10, undefined, decodedToken.userId).subscribe({
+      next: res => {
+        this.events = JSON.parse(JSON.stringify(res)).data;
+
+        for (let event in this.events){
+            let date = this.events[event].date;
+            date = new Date(date).toLocaleDateString('en-US'); 
+            this.events[event].date = date;
+        }
+      }, 
+      error: err => {
+        alert("Couldn't retrieve events" );
+      }
+    });
+  }
 
   courses = [
     {code: 'CSCC01', name: 'Introduction to Software Engineering', url: '#'},
