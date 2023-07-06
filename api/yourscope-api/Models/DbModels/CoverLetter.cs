88928a15cf4acfb6166be7be87da30b1ec984617
Cosmo48 <59401027+Cosmo48@users.noreply.github.com>
2023-07-05 21:37:09 -0400

    created student profile, cover letter, experience objects

---
+++ api/yourscope-api/Models/DbModels/CoverLetter.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace yourscope_api.Models.DbModels
+{
+    // Based on this format https://www.colorado.edu/business/sites/default/files/attached-files/Cover%20Letter%20Writing%202014.pdf
+    public class CoverLetter
+    {
+        [Key]
+        public int ResumeId { get; set; }
+        public required string Intro { get; set; }
+        public required string SalesPitch1 { get; set; }
+        public string? SalesPitch2 { get; set; } 
+        public string? SalesPitch3 { get; set; } //Optional since students may not have 2-3 skills to pitch to employers, but they should have at least 2
+        public required string Conclusion { get; set; }
+        public required ICollection<Experience> Experiences { get; set; }
+
+    }
+}
