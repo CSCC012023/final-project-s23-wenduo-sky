8659e589e528f4df40987018550eb591a53b880a
Ethan Zhang <ethanzhang57@gmail.com>
2023-07-06 11:13:39 -0400

    feat: Front-end UI to display events on student dashboard.

--- yourscope/src/app/student/dashboard-student/dashboard-student.component.ts
+++ yourscope/src/app/student/dashboard-student/dashboard-student.component.ts
@@ -9,14 +9,7 @@ import { APIService } from 'src/app/services/api.service';
 })
 export class DashboardStudentComponent implements OnInit {
   collapsed = true;
-  events = [
-    {title: 'Event', time: 'NOW', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
-    {title: 'Event', time: 'NOW', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
-    {title: 'Event', time: 'NOW', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
-    {title: 'Event', time: 'NOW', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
-    {title: 'Event', time: 'NOW', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
-    {title: 'Event', time: 'NOW', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
-  ];
+  events: any = [];
   jobs: any = [];
   currentCourses = [
     {code: 'CSCC01', name: 'Introduction to Software Engineering'},
@@ -38,10 +31,14 @@ export class DashboardStudentComponent implements OnInit {
   constructor(private api: APIService) { }
 
   ngOnInit() {
-    const url = 'https://localhost:7184/api/job/v1/posting?count=10';
-    this.api.get(url).subscribe(res => {
-      this.jobs = res;
+    let url = 'https://localhost:7184/api/events/v1?count=10';
+    this.api.get(url).subscribe((res: any) => {
+      this.events = res.data;
       this.eventsWidth = this.eventsWidth * this.events.length;
+    });
+    url = 'https://localhost:7184/api/job/v1/posting?count=10';
+    this.api.get(url).subscribe((res: any) => {
+      this.jobs = res.data;
       this.jobsWidth = this.jobsWidth * this.jobs.length;
     });
   }
