16bd60492908c9463be8aeaeb5c661e3a6fcf092
Cosmo48 <59401027+Cosmo48@users.noreply.github.com>
2023-07-11 15:07:42 -0400

    feat: addded backend for student profile

---
+++ api/yourscope-api/ServiceInterfaces/IProfileService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using yourscope_api.entities;
+using yourscope_api.Models.DbModels;
+using yourscope_api.Models.Request;
+
+namespace yourscope_api.service
+{
+    public interface IProfileService
+    {
+        public Profile? GetProfile(int UserId);
+        public int CreateProfile(ProfileCreation details);
+        public void ModifyProfile(ProfileDetails details);
+        public int CreateExperience(Experience experience);
+        public void DeleteExperience(int experienceId);
+        public int CreateCoverLetter(CoverLetter coverLetter);
+    }
+}
+
