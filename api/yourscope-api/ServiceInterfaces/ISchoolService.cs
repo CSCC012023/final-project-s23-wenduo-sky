04ef253c830c805802d54959abe2ad71db2fd556
YungCos <105815942+YungCos@users.noreply.github.com>
2023-07-11 18:36:07 -0400

    feat: endpoint to add courses (#89)

--- api/yourscope-api/ServiceInterfaces/ISchoolService.cs
+++ api/yourscope-api/ServiceInterfaces/ISchoolService.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using yourscope_api.entities;
+using yourscope_api.Models.DbModels;
 
 namespace yourscope_api.service
 {
     public interface ISchoolService
     {
         public ApiResponse GetSchoolsMethod();
+        public void PopulateCourseData(List<Course> courses, int schoolId);
     }
 }
 
--- api/yourscope-api/Services/SchoolService.cs
+++ api/yourscope-api/Services/SchoolService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using yourscope_api.entities;
 using yourscope_api.Models.DbModels;
 
@@ -22,6 +24,38 @@ namespace yourscope_api.service
             return new ApiResponse(StatusCodes.Status200OK, data: schools);
         }
 
+        public void PopulateCourseData(List<Course> courses, int schoolId)
+        {
+            using var context = new YourScopeContext();
+
+            var courseData = context.Courses.Select(q => q.CourseCode).ToHashSet();
+            var currCourses = context.SchoolCourse.Where(q => q.SchoolId == schoolId).Include(q => q.Course).Select(q => q.Course.CourseCode).ToHashSet();
+
+            int courseId;
+            foreach (Course course in courses)
+            {
+                if (courseData.Contains(course.CourseCode))
+                {
+                    if (currCourses.Contains(course.CourseCode))
+                    {
+                        throw new BadHttpRequestException($"Course with code {course.CourseCode} already belongs to the school");
+                    }
+                    // Get id of existing course in 
+                    courseId = context.Courses.First(q => q.CourseCode == course.CourseCode).CourseId;
+                }
+                else
+                {
+                    //Create new course
+                    context.Courses.Add(course);
+                    context.SaveChanges();
+                    courseId = course.CourseId;
+                }
+
+                context.SchoolCourse.Add(new SchoolCourse { CourseId = courseId , SchoolId = schoolId});
+            }
+            context.SaveChanges();
+        }
+
         #region helpers
         private static List<School> GetAllSchools()
         {
@@ -29,6 +63,7 @@ namespace yourscope_api.service
 
             return context.Schools.ToList();
         }
+
         #endregion
     }
 }
