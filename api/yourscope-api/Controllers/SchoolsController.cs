7cc24fd31a612aa823ac627ca8c4e534baa81d6c
Jason8908 <jasonsu894@gmail.com>
2023-07-11 20:59:03 -0400

    YS-138 feat: added endpoint to retrieve paginated filtered list of courses.

--- api/yourscope-api/Controllers/SchoolsController.cs
+++ api/yourscope-api/Controllers/SchoolsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Newtonsoft.Json;
 using yourscope_api.Models.DbModels;
 using Google.Api.Gax;
+using yourscope_api.Models.Request;
 
 namespace yourscope_api.Controllers
 {
@@ -24,9 +25,9 @@ namespace yourscope_api.Controllers
         #endregion
 
         /// <summary>
-        /// Gets the number of registered schools in the system.
+        /// Gets a list of registered schools in the system.
         /// </summary>
-        /// <returns>The number of registered schools in the system.</returns>
+        /// <returns>A list of registered schools in the system.</returns>
         [ProducesResponseType(typeof(int), 200)]
         [ProducesResponseType(500)]
         [HttpGet]
@@ -53,18 +54,55 @@ namespace yourscope_api.Controllers
         [ProducesResponseType(201)]
         [ProducesResponseType(500)]
         [HttpPost]
-        [Route("add-courses/{schoolId}")]
-        public IActionResult CourseInit([FromBody] List<Course> courses, int schoolId)
+        [Route("{schoolID}/courses")]
+        public IActionResult CourseInit([FromBody] List<Course> courses, int schoolID)
         {
             try
             {
-                service.PopulateCourseData(courses, schoolId);
+                service.PopulateCourseData(courses, schoolID);
                 return StatusCode(201, new ApiResponse(201, success: true));
             }
             catch (Exception ex)
             {
                 return StatusCode(500, new ApiResponse(500, ex.Message, success: false));
             }
+        }
+
+
+        /// <summary>
+        /// Gets a filtered list of courses.
+        /// </summary>
+        /// <param name="schoolID">An optional integer parameter representing the ID of a school used to filter courses by school.</param>
+        /// <param name="searchQuery" example="ICS2O">An optional string parameter used to filter courses by both course code and course title.</param>
+        /// <param name="grade" example="9">An optional integer between 9 and 12 inclusive used to filter courses by grade.</param>
+        /// <param name="disciplines" example="Math,Science,Geography">An optional comma seperated list of strings used to filter courses by discipline.</param>
+        /// <param name="offset">An optional query parameter (integer) representing the number of records to skip (default: 0). Used for pagination.</param>
+        /// <param name="count">An optional query parameter (integer) representing the number of records to return (default: 10). Used for pagination.</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("courses")]
+        public async Task<IActionResult> GetCourses(int? schoolID, string? searchQuery, int? grade, string? disciplines, int offset = 0, int count = 10)
+        {
+            ApiResponse response;
+            try
+            {
+                CourseFilter filters = new()
+                {
+                    SchoolID = schoolID,
+                    SearchQuery = searchQuery,
+                    Grade = grade,
+                    Disciplines = disciplines,
+                    Offset = offset,
+                    Count = count
+                };
+
+                response = await service.GetCoursesMethod(filters);
+            }
+            catch(Exception ex)
+            {
+                response = new(StatusCodes.Status500InternalServerError, exception: ex);
+            }
+            return StatusCode(response.StatusCode, response);
         }
     }
 }
--- api/yourscope-api/ServiceInterfaces/ISchoolService.cs
+++ api/yourscope-api/ServiceInterfaces/ISchoolService.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using yourscope_api.entities;
 using yourscope_api.Models.DbModels;
+using yourscope_api.Models.Request;
 
 namespace yourscope_api.service
 {
     public interface ISchoolService
     {
         public ApiResponse GetSchoolsMethod();
+        public Task<ApiResponse> GetCoursesMethod(CourseFilter filters);
         public void PopulateCourseData(List<Course> courses, int schoolId);
     }
 }
