304bd7bf5eb5b068e365245c7c52ed821121c498
Jason8908 <jasonsu894@gmail.com>
2023-07-12 15:00:45 -0400

    YS-142 feat: added Schedule and Year tables to the database.

---
+++ api/yourscope-api/Models/DbModels/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace yourscope_api.Models.DbModels
+{
+    public class Schedule
+    {
+        [Key]
+        public int ScheduleId { get; set; }
+        public required int StudentId { get; set; }
+        [ForeignKey("StudentId")]
+        public required User Student { get; set; }
+        public List<Year>? Years { get; set; } = new List<Year>();
+    }
+}
