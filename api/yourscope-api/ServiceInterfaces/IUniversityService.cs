36bd1c126939549e31756af4f8343e48aef01c6e
Cosmo48 <59401027+Cosmo48@users.noreply.github.com>
2023-08-01 16:10:55 -0400

    feat: added endpoint to retrive uni info

---
+++ api/yourscope-api/ServiceInterfaces/IUniversityService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using yourscope_api.entities;
+using yourscope_api.Models.DbModels;
+using yourscope_api.Models.Request;
+
+namespace yourscope_api.service
+{
+    public interface IUniversityService
+    {
+        public List<UniProgram> GetPrograms(UniProgramDetails details, int count, int offset);
+        public List<University> GetUniversities();
+    }
+}
+
